295f63b120394a3d182a00683f88071ac8ca9331
Balazs Radvanyi <balazs@riltech.co>
2026-09-05 21:50:16 +0200

    feat: implement transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let currentState = { ...state };
+  const result = [];
+
+  for (const action of actions) {
+    let stateCopy = { ...currentState };
+
+    switch (action.type) {
+      case 'clear':
+        stateCopy = {};
+        break;
+
+      case 'addProperties':
+        stateCopy = { ...stateCopy, ...action.extraData };
+        break;
+
+      case 'removeProperties':
+        action.keysToRemove.forEach((key) => delete stateCopy[key]);
+        break;
+
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
+    }
+
+    currentState = stateCopy;
+    result.push(currentState);
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
